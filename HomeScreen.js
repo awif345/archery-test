a3f620577fb3374a991670ce0750b3ad432bc0f1
awif345 <alexwifler@gmail.com>
2024-05-24 16:10:08 -0500

    Create HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,13 @@
+// HomeScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
